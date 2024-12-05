fe3316654c5bbc992a03f4b9932d45ed332e52b8
ripnines <ripnines@outlook.com>
2024-12-04 21:07:39 -0500

    Update confirmemailjs.js

--- account/js/confirmemailjs.js
+++ account/js/confirmemailjs.js
@@ -26,7 +26,7 @@ import {
   
   
     //___________________________________________
-var email = document.getElementById('email').value;
+var email = document.getElementById('email');
  localStorage.removeItem('email');
 document.getElementById('message').textContent = 'A confirmation email has bent sent to ' + email;
      // ..

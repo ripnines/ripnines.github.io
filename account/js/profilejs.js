888d21f7fc2f798079d457477c60f0b8252bf92f
ripnines <ripnines@outlook.com>
2024-12-11 16:49:12 -0500

    Create profilejs.js

---
+++ account/js/profilejs.js
@@ -0,0 +1,81 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+  updateProfile,
+  connectAuthEmulator,
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDB-75ye1J71GxZFH2Gt1bBG89sNNVIey8",
+  authDomain: "ripnines-b7119.firebaseapp.com",
+  projectId: "ripnines-b7119",
+  storageBucket: "ripnines-b7119.firebasestorage.app",
+  messagingSenderId: "541327244207",
+  appId: "1:541327244207:web:2502bfb6ddfa1d25a6cd5a",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app);
+
+//------------------------------------------------
+
+
+
+//------------------------------------------------
+auth.onAuthStateChanged((user) => {
+
+  if (user) {
+  document.getElementById("name").textcontent = user.displayName;
+  
+    function updateprofile() {
+      user
+        .updateProfile({
+          displayName: document.getElementById("name").value,
+        })
+        .then(
+          function () {
+            var displayName = user.displayName
+            document.getElementById("name").textcontent = displayName;
+          },
+          
+          function (error) {
+           var errorCode = error.code;
+    var errorMessage = error.message;
+    console.error('Error signing in:', errorCode, errorMessage); 
+    var errorfortext = errorCode.replace("auth/", "").replace("-", " ");
+    document.getElementById('error').style.color = "#E81818";
+    document.getElementById('error').textContent = errorfortext;
+          
+          },
+        )
+    }
+    
+    document.getElementById('button').onclick = function() {
+    console.log("he;;p")
+updateprofile()
+}
+    
+    
+  } else {
+  
+  
+    console.log("not signedin");
+    document.getElementById('error').style.color = "#E81818";
+    document.getElementById('error').textContent = "not signed in"
+    localStorage.setItem('link', window.location.href); 
+  //  window.location.replace("https://ripnines.github.io/account/signin");
+    
+  }
+  
+
+  
+})
+
+ 
+

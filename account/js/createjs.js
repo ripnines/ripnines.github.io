ea1a1eb45e66839309b1290eb912f5f2b1f5c7bd
ripnines <ripnines@outlook.com>
2024-12-26 21:12:54 -0500

    Update createjs.js

--- account/js/createjs.js
+++ account/js/createjs.js
@@ -1,78 +1,86 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
 import {
-  getAuth,
-  onAuthStateChanged,
+ getAuth,
+  onAuthStateChanged, 
   signOut,
   createUserWithEmailAndPassword,
   connectAuthEmulator,
-  sendEmailVerification,
-  updateProfile,
-} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"
+  sendEmailVerification
+} from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDB-75ye1J71GxZFH2Gt1bBG89sNNVIey8",
-  authDomain: "ripnines-b7119.firebaseapp.com",
-  projectId: "ripnines-b7119",
-  storageBucket: "ripnines-b7119.firebasestorage.app",
-  messagingSenderId: "541327244207",
-  appId: "1:541327244207:web:2502bfb6ddfa1d25a6cd5a",
-}
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDB-75ye1J71GxZFH2Gt1bBG89sNNVIey8",
+    authDomain: "ripnines-b7119.firebaseapp.com",
+    projectId: "ripnines-b7119",
+    storageBucket: "ripnines-b7119.firebasestorage.app",
+    messagingSenderId: "541327244207",
+    appId: "1:541327244207:web:2502bfb6ddfa1d25a6cd5a"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const auth = getAuth(app)
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
 
-//___________________________________________
+  
+    //___________________________________________
+    
 
-//___________________________________________
+      
+    //___________________________________________
 
-function emailVeri() {
-  sendEmailVerification(auth.currentUser).then(() => {})
-}
+  
+    function emailVeri() {
+   sendEmailVerification(auth.currentUser)
+    .then(() => {
+  
+    });
 
-function signUpEmail(email, password) {
+  }
+  
+   function signUpEmail(email,password) {
+  
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
+  .then((userCredential) => {
+    // Signed in 
+    
+    var user = userCredential.user;
+    
+    emailVeri()
+    localStorage.setItem('email', document.getElementById('email').value); 
+    window.location.href = 'https://ripnines.github.io/account/confirmemail';
 
-      var user = userCredential.user
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.error('Error signing in:', errorCode, errorMessage); 
+    var errorfortext = errorCode.replace("auth/", "").replace("-", " ");
+    document.getElementById('error').style.color = "#E81818";
+    document.getElementById('error').textContent = errorfortext;
+  });
+  
+  }
+  
 
-      updateProfile(user, {
-        displayName: document.getElementById("name").value,
-      })
-        .then(() => {
-          var displayName = user.displayName
-          document.getElementById("name").textContent = displayName
-        })
-        .catch((error) => {
-          var errorCode = error.code
-          var errorMessage = error.message
-        })
+    
+  //___________________________________________
+  
+  document.getElementById('signbutton').onclick = function() {
+  
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  signUpEmail(email, password);
+  
+};
+  
+  
 
-      emailVeri()
-      localStorage.setItem("email", document.getElementById("email").value)
-      signOut(auth)
-      window.location.replace("https://ripnines.github.io/account/confirmemail")
-
-      // ...
-    })
-    .catch((error) => {
-      var errorCode = error.code
-      var errorMessage = error.message
-      console.error("Error signing in:", errorCode, errorMessage)
-      var errorfortext = errorCode.replace("auth/", "").replace("-", " ")
-      document.getElementById("error").style.color = "#E81818"
-      document.getElementById("error").textContent = errorfortext
-    })
-}
-
-//___________________________________________
-
-document.getElementById("signbutton").onclick = function () {
-  var email = document.getElementById("email").value
-  var password = document.getElementById("password").value
-
-  signUpEmail(email, password)
-}
+  
+  
+  
+  
+  
